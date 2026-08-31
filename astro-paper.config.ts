71dfd47373bcc3df0979dcd3da48a5aabd7d433f
yeonamn624 <yeonamn624@gmail.com>
2026-09-01 01:36:58 +0900

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,6 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: false,
-      url: "https://github.com/yeonamn624/yeonamn624.github.io/edit/main/",
     },
     search: "pagefind",
   },
